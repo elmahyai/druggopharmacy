15a620360ee55d9265d370634c99e425df26ad13
ahmedelmahy <elmahy2005@gmail.com>
2020-01-06 19:46:13 +0200

    using ghost wordpress

--- README.md
+++
@@ -1,2 +0,0 @@
-# druggopharmacy
-MERN application - skills showcase and related to druggo startup
--- routes/serve_static_assets.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const router = require('express').Router();
-const express = require('express');
-
-const publicDir = path.join(__dirname, 'public');
-
-router.get(express.static(publicDir));
